9298ac0c3eb76bb6c65279ccdb928b809e45b7b2
AMAD97 <aminedaoui00@gmail.com>
2023-09-08 08:49:52 -0700

    zero positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,25 +11,23 @@ int main(void)
 {
 int n; /* Declare the variable at the beginning of the block */
 
-/* Seed the random number generator */
 srand(time(NULL));
 
-/* Generate a random number between -100 and 100 (inclusive) */
-n = (rand() % 201) - 100;
+n = rand()-RAND_MAX/2;
 
 /* Check if n is positive, negative, or zero */
 printf("%d is ", n);
 if (n > 0)
 {
-printf("positive\n");
+printf("positive\n".n);
 }
 else if (n < 0)
 {
-printf("negative\n");
+printf("negative\n.n");
 }
 else
 {
-printf("zero\n");
+printf("zero\n.n");
 }
 
 return (0);
