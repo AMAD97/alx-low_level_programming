99f70251177671d18856f5456ca9a29626d32af6
AMAD97 <aminedaoui00@gmail.com>
2023-09-09 10:01:43 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#iclude <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char l  = 'a';
+	while (l  <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	l = 'A';
+	while (l <= 'Z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar("\n");
+	return (0);
+}
+	
