7d8544c955a4658aa7dee8424e662b25089615d6
AMAD97 <aminedaoui00@gmail.com>
2023-09-13 11:08:22 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,11 @@ void times_table(void)
 			if (j == 0)
 			{
 				_putchar('0');
-		
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				
 			if (k < 10)
 			{
 				_putchar(' ');
@@ -32,7 +29,6 @@ void times_table(void)
 			{
 			_putchar(k / 10  + '0');
 			}
-			
 			_putchar(k % 10 + '0');
 			}
 		}
