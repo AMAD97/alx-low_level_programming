77126a67a3015be577f7c3af5887d07b9870f74b
AMAD97 <aminedaoui00@gmail.com>
2023-09-14 11:44:10 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,26 +6,26 @@
  * @n: number of times the character \
  * should be printed
  */
-
 void print_diagonal(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-} 
-else
-{
-int i, j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-if (j == i)
-_putchar('\\');
-else if (j < i)
-_putchar(' ');
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        int i, j;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (j == i)
+                    _putchar('\\');
+                else if (j < i)
+                    _putchar(' ');
+            }
+            _putchar('\n'); /* The diagonal should end with a \n */
+        }
+    }
 }
-_putchar('\n'); /* The diagonal should end with a \n */
-}
-}
-}
+
