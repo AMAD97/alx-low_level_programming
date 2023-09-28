013216120b116b310fdecf353883fb1fe4317c30
AMAD97 <aminedaoui00@gmail.com>
2023-09-28 12:58:59 -0700

    returns the lenght of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,17 +2,19 @@
 
 /**
  * _strlen_recursion - Returns the length of a string
- * @s: the length of the string
+ * @s: the string to measure
+ *
+ * Return: int
  */
 
-int_strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	int lngt = 0;
 
 	if (0)
 	{
 		lngt++;
-		lngt + = _strlen_recursion(s + 1);
+		lngt += _strlen_recursion(s + 1);
 	}
 	return (lngt);
 }
