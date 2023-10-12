f508d828f6560bff82b819802ccc2c4b2dce6326
AMAD97 <aminedaoui00@gmail.com>
2023-10-11 17:38:33 -0700

    OPCODES

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,36 +9,18 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+nt main(int argc, char **argv)
 {
-	int bytes, i;
-	char *arr;
+char *p = (char *)main;
+int b;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+if (argc != 2)
+printf("Error\n"), exit(1);
+b = atoi(argv[1]);
+if (b < 0)
+printf("Error\n"), exit(2);
 
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx", arr[i]);
-	}
-	return (0);
+while (b--)
+printf("%02hhx%s", *p++, b ? " " : "\n");
+return (0);
 }
-
